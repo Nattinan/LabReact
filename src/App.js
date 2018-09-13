07c9d54208e179e9b1866b5f8a4eb7001661d62d
Nattinan <nattinan001@gmail.com>
2018-09-13 10:43:29 +0700

    editApp2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import CharacterCard from './CharacterCard';
 
   
+cconst word = "Hello";
 class App extends Component {
  render() {
  return (
- 
-  <div>
-  <CharacterCard value="h"/>
- <CharacterCard value="i"/>
-  </div>
-
- 
+ <div>
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
  );
  }
 }
+
  
 export default App;
