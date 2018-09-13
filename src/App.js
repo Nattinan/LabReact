cb79bdc1b0928896eccb3ccf0b4e8f82bd52ee3b
Nattinan <nattinan001@gmail.com>
2018-09-13 10:38:31 +0700

    Helloworld

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+//import CharacterCard from './CharacterCard';
 
   
 class App extends Component {
  render() {
  return (
- <div>
- <CharacterCard value="h"/>
-<CharacterCard value="i"/>
+ 
+ <div className="App">
+ Hello World
  </div>
+
+ 
  );
  }
 }
